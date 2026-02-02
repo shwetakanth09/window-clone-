2e00fdb955fda05e5bbecdde90aa115ecdb1a282
shwetakanth09 <shwetakanth5@gmail.com>
2026-02-02 16:16:16 +0530

    feat: add drag and drop support in helpers

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,3 +1,3 @@
-// helpers - 2026-01-13
-export const helpersVersion = '6';
-export const lastUpdated = '2026-01-13';
+// helpers - 2026-02-02
+export const helpersVersion = '16';
+export const lastUpdated = '2026-02-02';
