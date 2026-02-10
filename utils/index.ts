9669bdb479935c1547db7fcbe2b62614ba222ce6
shwetakanth09 <shwetakanth5@gmail.com>
2026-02-10 10:20:20 +0530

    fix: handle touch events on mobile in utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,3 @@
-// utils - 2026-01-21
-export const utilsVersion = '10';
-export const lastUpdated = '2026-01-21';
+// utils - 2026-02-10
+export const utilsVersion = '20';
+export const lastUpdated = '2026-02-10';
