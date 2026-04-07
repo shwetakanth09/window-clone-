477fad4089024f12a4e2d7078d2921df68f973a3
shwetakanth09 <shwetakanth5@gmail.com>
2026-04-07 18:48:48 +0530

    feat: add responsive grid layout in widgets

--- widgets/index.ts
+++ widgets/index.ts
@@ -1,3 +1,3 @@
-// widgets - 2026-03-18
-export const widgetsVersion = '38';
-export const lastUpdated = '2026-03-18';
+// widgets - 2026-04-07
+export const widgetsVersion = '48';
+export const lastUpdated = '2026-04-07';
