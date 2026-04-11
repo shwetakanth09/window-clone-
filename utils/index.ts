aca4f88709874bb07d733cd8d4fadfbb4340a236
shwetakanth09 <shwetakanth5@gmail.com>
2026-04-11 10:50:50 +0530

    feat: implement smooth scroll behavior in utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,3 @@
-// utils - 2026-03-22
-export const utilsVersion = '40';
-export const lastUpdated = '2026-03-22';
+// utils - 2026-04-11
+export const utilsVersion = '50';
+export const lastUpdated = '2026-04-11';
