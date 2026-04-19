5bb765b046b9ce1b404e2bc0ebc4aa003c3fbcf8
shwetakanth09 <shwetakanth5@gmail.com>
2026-04-19 14:54:54 +0530

    fix: correct padding on mobile view in config

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
-// config - 2026-03-30
-export const configVersion = '44';
-export const lastUpdated = '2026-03-30';
+// config - 2026-04-19
+export const configVersion = '54';
+export const lastUpdated = '2026-04-19';
