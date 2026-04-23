30627567d95076aec7948c6681351a29949afa47
shwetakanth09 <shwetakanth5@gmail.com>
2026-04-23 16:56:56 +0530

    feat: add dark mode support in helpers

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,3 +1,3 @@
-// helpers - 2026-04-03
-export const helpersVersion = '46';
-export const lastUpdated = '2026-04-03';
+// helpers - 2026-04-23
+export const helpersVersion = '56';
+export const lastUpdated = '2026-04-23';
