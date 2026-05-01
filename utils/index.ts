a3f11cb1a8fa762b2ee7836bf07d656b99c5bdf2
shwetakanth09 <shwetakanth5@gmail.com>
2026-05-01 10:00:00 +0530

    fix: prevent double form submission in utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,3 @@
-// utils - 2026-04-11
-export const utilsVersion = '50';
-export const lastUpdated = '2026-04-11';
+// utils - 2026-05-01
+export const utilsVersion = '60';
+export const lastUpdated = '2026-05-01';
