e76c4e00afddbf912c95a53bae517ca688dbaea3
shwetakanth09 <shwetakanth5@gmail.com>
2026-05-21 10:10:10 +0530

    chore: migrate to latest framework version in utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,3 @@
-// utils - 2026-05-01
-export const utilsVersion = '60';
-export const lastUpdated = '2026-05-01';
+// utils - 2026-05-21
+export const utilsVersion = '70';
+export const lastUpdated = '2026-05-21';
