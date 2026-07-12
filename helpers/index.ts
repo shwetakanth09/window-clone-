8d015794b8cea3d509d0c48d42662e0f7844780c
shwetakanth09 <shwetakanth5@gmail.com>
2026-07-12 16:36:36 +0530

    feat: add responsive grid layout in helpers

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,3 +1,3 @@
-// helpers - 2026-06-22
-export const helpersVersion = '86';
-export const lastUpdated = '2026-06-22';
+// helpers - 2026-07-12
+export const helpersVersion = '96';
+export const lastUpdated = '2026-07-12';
